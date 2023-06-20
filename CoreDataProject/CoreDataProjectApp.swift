54d9f3c1d5aba92548cb0d4a88b6749fa514fc49
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-20 20:19:56 +0530

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Rishav Gupta on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
